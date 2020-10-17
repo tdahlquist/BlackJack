6fb3adbcac4c05bdd6742ba4f88b6532d3709370
Ichi-Moku <71863018+tdahlquist@users.noreply.github.com>
2020-10-16 23:44:27 -0500

    Update BJGame.py

--- BJGame.py
+++ BJGame.py
@@ -3,9 +3,14 @@ import random
 import sys
 
 ####### Variables
-deck = [1, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 10, 10, 10]
-chipCount, minBet, maxBet = 100, 5, 100
+deck = {'A - hearts': 1,'2 - hearts': 2,'3 - hearts': 3,'4 - hearts': 4,'5 - hearts': 5,'6 - hearts': 6,'7 - hearts': 7 ,'8 - hearts': 8,'9 - hearts': 9,'10 - hearts': 10,'J - hearts': 10,'Q - hearts': 10,'K - hearts': 10,
+        'A - Clubs': 1,'2 - Clubs': 2,'3 - Clubs': 3,'4 - Clubs': 4,'5 - Clubs': 5,'6 - Clubs': 6,'7 - Clubs': 7 ,'8 - Clubs': 8,'9 - Clubs': 9,'10 - Clubs': 10,'J - Clubs': 10,'Q - Clubs': 10,'K - Clubs': 10,
+        'A - Spades': 1,'2 - Spades': 2,'3 - Spades': 3,'4 - Spades': 4,'5 - Spades': 5,'6 - Spades': 6,'7 - Spades': 7 ,'8 - Spades': 8,'9 - Spades': 9,'10 - Spades': 10,'J - Spades': 10,'Q - Spades': 10,'K - Spades': 10,
+        'A - Diamonds': 1,'2 - Diamonds': 2,'3 - Diamonds': 3,'4 - Diamonds': 4,'5 - Diamonds': 5,'6 - Diamonds': 6,'7 - Diamonds': 7 ,'8 - Diamonds': 8,'9 - Diamonds': 9,'10 - Diamonds': 10,'J - Diamonds': 10,'Q - Diamonds': 10,'K - Diamonds': 10,
+}
 
+chipCount, minBet, maxBet = 100, 5, 100
+cardAndValue = list(deck.items())
 
 ###### Functions
 
@@ -38,14 +43,19 @@ def ask(userInput):
 
 # Need to play out dealers hand as needed
 def dealerHitCheck():
-    print('Dealer flips second card and shows he has a ' + str(dealerHand[0]) + ' and a ' + str(dealerHand[1]) + ' for a total of: ' + str(sum(dealerHand)) + '\n')
+    card, cValue = random.choice(cardAndValue)    
+    dealerHand.setdefault(card,cValue)
+    newDeck.pop(card)
+    print('Dealer flips second card and is showing ' + str(dealerHand.keys()) + ' for a total of: ' + str(sum(dealerHand.values())) + '\n')
     while True:
-        dh = sum(dealerHand)
+        dh = sum(dealerHand.values())
         if dh > 16:
             return dh
         else:
-            dealerHand.append(random.choice(deck))
-            print('Dealer hits and now has: ' + str(sum(dealerHand)) + '\n')
+            card, cValue = random.choice(cardAndValue)    
+            dealerHand.setdefault(card,cValue)
+            newDeck.pop(card)
+            print('Dealer hits and now has a total of: ' + str(sum(dealerHand.values())) + '\n')
 
 def playerHit():
     cardNum = 3
@@ -96,26 +106,33 @@ print('Hi ' + name + ', have not seen you in a while, pull up a chair! We are pl
 
 # Game starts here
 while True:
+    newDeck = deck # Set's the new deck - each game uses a freshly shuffled deck.
+    
     print('You currently have $' + str(chipCount))
 
     # Place and check if bet is legal
     bet = placeBet()
 
     # flop initial cards into a list called flop
-    playerHand = []
-    dealerHand = []
-    for cards in range(2):
-        playerHand.append(random.choice(deck))
-        dealerHand.append(random.choice(deck))
-    print('You show a ' + str(playerHand[0]) + ' and a ' + str(playerHand[1]))
-    print('The dealer is showing a ' + str(dealerHand[0]) + '\n')
+    playerHand = {}
+    dealerHand = {}
+    for i in range(3):
+        card, cValue = random.choice(cardAndValue)
+        if (i+2) % 2 == 0:
+            playerHand.setdefault(card,cValue)
+            newDeck.pop(card)
+        else:
+            dealerHand.setdefault(card,cValue)
+            newDeck.pop(card) 
+    print('You are showing the two cards ' + str(playerHand.keys()))
+    print('The dealer is showing a ' + str(dealerHand.keys()) + '\n')
 
     # Ask P1 what they would like to do first do if stay
     hitStay = ask(input('What would you like to do? Hit or Stay? (Type H or S): '))
     print('You decided to - ' + hitStay)
-    if hitStay == 'Stay':
+    if hitStay == 'Stay':       
         dh = dealerHitCheck() # Dealer needs to hit if necessary
-        gameStatus = whoWon(sum(playerHand),dh)
+        gameStatus = whoWon(sum(playerHand.values()),dh)
     else:
         ph = playerHit() # player hits
         dh = dealerHitCheck() # Dealer needs to hit if necessary
